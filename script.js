e8176ebcd7d230d3697001b667134fe46e0ac48f
aryanmotghare77 <89638995+aryanmotghare77@users.noreply.github.com>
2021-10-05 16:53:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleHide() {
+    let btn = document.getElementById('btn')
+    let para = document.getElementById('para');
+    if(para.style.display!='none') {
+        para.style.display='none'
+    }
+    else{
+        para.style.display = 'block'
+    }
+}
